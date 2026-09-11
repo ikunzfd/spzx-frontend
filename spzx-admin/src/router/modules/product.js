aea7377e75a0b47f8302c67e451c74f29a5f0517
zfd <2111651970@qq.com>
2026-09-11 20:59:14 +0800

    完成品牌分类功能

---
+++ spzx-admin/src/router/modules/product.js
@@ -0,0 +1,42 @@
+const Layout = () => import('@/layout/index.vue')
+const category = () => import('@/views/product/category.vue')
+const brand = () => import('@/views/product/brand.vue')
+const categoryBrand = () => import('@/views/product/categoryBrand.vue')
+
+export default [
+  {
+    path: '/product',
+    component: Layout,
+    name: 'product',
+    meta: {
+      title: '商品管理',
+    },
+    icon: 'Histogram',
+    children: [
+      {
+        path: '/category',
+        name: 'category',
+        component: category,
+        meta: {
+          title: '分类管理',
+        },
+      },
+      {
+        path: '/brand',
+        name: 'brand',
+        component: brand,
+        meta: {
+          title: '品牌管理',
+        },
+      },
+      {
+        path: '/categoryBrand',
+        name: 'categoryBrand',
+        component: categoryBrand,
+        meta: {
+          title: '分类品牌',
+        },
+      },
+    ],
+  },
+]
